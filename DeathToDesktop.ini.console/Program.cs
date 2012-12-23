8626fb1a355b85b55178f8cbe31637cc51b6cec8
Ian Bond <ibond@onezero.com>
2012-12-23 02:34:09 -0500

    Initial commit.

---
+++ DeathToDesktop.ini.console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeathToDesktop.ini.console
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                var watcher = new LibDeathToDesktop.ini.DeathToDesktopIniWatcher();
+                Thread.Sleep(Timeout.Infinite);
+
+                return 0;
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Failed to create directory watcher: {0}", exp.Message);
+                return 1;
+            }
+        }
+    }
+}
